9a2cef06c95259776966f467b20c0fb624a2b9f7
abhitrivedi99 <abhitrivedi99@gmail.com>
2021-09-23 01:29:58 +0530

    small refactoring

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -24,17 +24,9 @@ interface HasId {
 export class Model<T extends HasId> {
     constructor(private attributes: ModelAttributes<T>, private events: Events, private sync: Sync<T>) {}
 
-    get on() {
-        return this.events.on
-    }
-
-    get trigger() {
-        return this.events.trigger
-    }
-
-    get get() {
-        return this.attributes.get
-    }
+    on = this.events.on
+    trigger = this.events.trigger
+    get = this.attributes.get
 
     set(update: T): void {
         this.attributes.set(update)
