50b74e256ff62ba83e4a559b32abf24979f83ee0
abhitrivedi99 <abhitrivedi99@gmail.com>
2021-09-24 23:21:53 +0530

    View added for user collection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-import { UserEdit } from './views/UserEdit'
-import { User } from './models/User'
+// import { UserEdit } from './views/UserEdit'
+// import { User } from './models/User'
+//
+// const user = User.buildUser({ name: 'NAME', age: 20 })
+// const root = document.getElementById('root')
+//
+// if (root) {
+//     const userEdit = new UserEdit(root, user)
+//     userEdit.render()
+// } else {
+//     throw Error('Root element not found')
+// }
+import { Collection } from './models/Collection'
+import { User, UserProps } from './models/User'
+import { UserList } from './views/UserList'
 
-const user = User.buildUser({ name: 'NAME', age: 20 })
-const root = document.getElementById('root')
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+    return User.buildUser(json)
+})
 
-if (root) {
-    const userEdit = new UserEdit(root, user)
-    userEdit.render()
-} else {
-    throw Error('Root element not found')
-}
+users.on('change', () => {
+    const root = document.getElementById('root')
+
+    if (root) {
+        new UserList(root, users).render()
+    }
+})
+
+users.fetch()
