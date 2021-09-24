e80a2e37d3d8ab0319b1aa857e17be71aa31bda4
abhitrivedi99 <abhitrivedi99@gmail.com>
2021-09-24 12:15:35 +0530

    Added views

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { User } from './models/User'
+import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const collection = User.buildUserCollection()
+const user = User.buildUser({ name: 'NAME', age: 20 });
 
-collection.on('change', () => console.log(collection))
+const userForm = new UserForm(document.getElementById('root'), user);
 
-collection.fetch()
+userForm.render();
